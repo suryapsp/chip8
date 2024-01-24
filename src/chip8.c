c9673eefb71079a526f0892e5fbfc16b1441b4b6
suryapsp <techie-human@protonmail.com>
2024-01-24 20:34:12 +0530

    window config

--- src/chip8.c
+++ src/chip8.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 #include "SDL.h"
+
+typedef struct {
+	uint32_t window_width;
+	uint32_t window_height;
+}config_t;
 
 typedef struct 
 {
@@ -10,30 +16,59 @@ typedef struct
 }sdl_t;
 
 
-bool init_sdl(void){
+bool init_sdl(sdl_t *sdl, config_t config){
 	if(SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO|SDL_INIT_TIMER) != 0){
 		SDL_Log("Can't Initialize SDL Subsystem %s \n", SDL_GetError());
 		return false; // Initialization Failed
 	}
 
+
+	sdl -> window = SDL_CreateWindow("CHIP-8 Emulator", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, config.window_width, config.window_height, 0);
+
+	if(!sdl -> window){
+		SDL_Log("could not create window %s\n", SDL_GetError());
+	}
+
 	return true; // Initialization Done
 }
 
-void final_cleanup(void){
-	// SDL_DestroyWindow(window);
+// Initial Emulator Config from actual args
+bool set_config(config_t *config, int argc, char **argv){
+	// Defaults
+	*config = (config_t){
+		.window_width = 64,
+		.window_height = 32
+	};
+
+	// Change Defaults
+	for(int i = 0; i < argc; i++){
+		(void)argv[i];
+	}
+
+	return true;
+}
+
+void final_cleanup(sdl_t sdl){
+	SDL_DestroyWindow(sdl.window);
 	SDL_Quit(); // Quit SDL Subsystem
 }
 
 int main(int argc, char **argv){
-	(void) argc;
-	(void) argv;
-
-	sdl_t sdl = {0};
-	if(init_sdl() == false){
+	// Emulator Config
+	config_t config = {0};
+	if(set_config(&config, argc, argv) == false){
 		exit(EXIT_FAILURE);
 	}
 
-	final_cleanup();
+	// Initialization
+	sdl_t sdl = {0};
+	if(init_sdl(&sdl, config) == false){
+		exit(EXIT_FAILURE);
+	}
+
+	
+
+	final_cleanup(sdl);
 
 	exit(EXIT_SUCCESS);
 }
