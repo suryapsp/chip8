634cc9fb8d3ad7dd4d62360a85f2c3255f1eb799
suryapsp <techie-human@protonmail.com>
2024-01-25 22:44:58 +0530

    added sdl renderer

--- src/chip8.c
+++ src/chip8.c
@@ -8,11 +8,14 @@
 typedef struct {
 	uint32_t window_width;
 	uint32_t window_height;
+	uint32_t foreground_color; // foreground color R-8 G-8 B-8 A-8
+	uint32_t background_color; // bg color R-8 G-8 B-8 A-8
 }config_t;
 
 typedef struct 
 {
 	SDL_Window *window;
+	SDL_Renderer *renderer;
 }sdl_t;
 
 
@@ -29,6 +32,12 @@ bool init_sdl(sdl_t *sdl, config_t config){
 		SDL_Log("could not create window %s\n", SDL_GetError());
 	}
 
+	sdl -> renderer = SDL_CreateRenderer(sdl -> window, -1, SDL_RENDERER_ACCELERATED);
+
+	if(!sdl->renderer){
+		SDL_Log("could not create renderer %s\n", SDL_GetError());
+	}
+
 	return true; // Initialization Done
 }
 
@@ -37,7 +46,9 @@ bool set_config(config_t *config, int argc, char **argv){
 	// Defaults
 	*config = (config_t){
 		.window_width = 64,
-		.window_height = 32
+		.window_height = 32, //OG CHIP8 Resolution
+		.foreground_color = 0xFFFF00FF, //Yellow 
+		.background_color = 0x00000000  //Black
 	};
 
 	// Change Defaults
@@ -49,8 +60,23 @@ bool set_config(config_t *config, int argc, char **argv){
 }
 
 void final_cleanup(sdl_t sdl){
+	SDL_DestroyRenderer(sdl.renderer);
 	SDL_DestroyWindow(sdl.window);
 	SDL_Quit(); // Quit SDL Subsystem
+}
+
+// Clear Screen
+void clear_screen(const config_t config){
+	const uint8_t r = (config.background_color >> 24) & 0xFF;
+	const uint8_t g = (config.background_color >> 16) & 0xFF;
+	const uint8_t b = (config.background_color >> 8) & 0xFF;
+	const uint8_t a = (config.background_color >> 0) & 0xFF;
+	SDL_SetRenderDrawColor(sdl.renderer, r,g,b,a);
+	SDL_RenderClear(sdl.renderer);
+}
+
+void update_screen(const config_t config){
+	
 }
 
 int main(int argc, char **argv){
@@ -66,7 +92,15 @@ int main(int argc, char **argv){
 		exit(EXIT_FAILURE);
 	}
 
-	
+
+
+	// Main Emulator Loop
+	while(true){
+		// Delay for 60fps
+		SDL_Delay();
+		// Update Window
+		update_screen(config);
+	}
 
 	final_cleanup(sdl);
 
