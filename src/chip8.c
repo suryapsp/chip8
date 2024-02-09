2ba9ad03943d94ff2487196bfba489813731e8d6
suryapsp <techie-human@protonmail.com>
2024-02-09 20:37:48 +0530

    added FX0A opcode and finished the keypad part

--- src/chip8.c
+++ src/chip8.c
@@ -336,6 +336,77 @@ void handle_input(chip8_t *chip8){
 				break;
 
 			case SDL_KEYUP:
+				switch(event.key.keysym.sym){
+
+					case SDLK_1:
+						chip8->keypad[0x1] =  false;
+						break;
+
+					case SDLK_2:
+						chip8->keypad[0x2] =  false;
+						break;
+
+					case SDLK_3:
+						chip8->keypad[0x3] =  false;
+						break;
+
+					case SDLK_4:
+						chip8->keypad[0xC] =  false;
+						break;
+
+					case SDLK_q:
+						chip8->keypad[0x4] =  false;
+						break;
+
+					case SDLK_w:
+						chip8->keypad[0x5] =  false;
+						break;
+
+					case SDLK_e:
+						chip8->keypad[0x6] =  false;
+						break;
+
+					case SDLK_r:
+						chip8->keypad[0xD] =  false;
+						break;
+
+					case SDLK_a:
+						chip8->keypad[0x7] =  false;
+						break;
+
+					case SDLK_s:
+						chip8->keypad[0x8] =  false;
+						break;
+
+					case SDLK_d:
+						chip8->keypad[0x9] =  false;
+						break;
+
+					case SDLK_f:
+						chip8->keypad[0xE] =  false;
+						break;
+
+					case SDLK_z:
+						chip8->keypad[0xA] =  false;
+						break;
+
+					case SDLK_x:
+						chip8->keypad[0x0] =  false;
+						break;
+
+					case SDLK_c:
+						chip8->keypad[0xB] =  false;
+						break;
+
+					case SDLK_v:
+						chip8->keypad[0xF] =  false;
+						break;
+
+
+					default:
+						break;
+
+				}
 				break;
 
 			default:
@@ -902,7 +973,20 @@ void emulate_instructions(chip8_t *chip8, const config_t config){
 				// 0xFX0A
 				// A key press is awaited, and then stored in VX (blocking operation, all instruction halted until next key event)
 
+				bool key_pressed = false;
 
+				for(uint8_t i = 0; i < sizeof(chip8->keypad); i++){
+					if(chip8->keypad[i]){
+						chip8->V[chip8->inst.X] = i;
+						key_pressed = true;
+						break;
+					}
+				}
+
+				// keep getting the current opcode and running the this instruction until a key is pressed
+				if(key_pressed == false){
+					chip8->PC -= 2;
+				}
 				
 				break;
 			
